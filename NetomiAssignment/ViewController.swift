726ae37aeaa1dd3db57429507fbc6f025209a4b5
Kirti S <shilwantkirti@gmail.com>
2025-05-05 10:28:57 +0530

    Initial Commit

---
+++ NetomiAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetomiAssignment
+//
+//  Created by Kirti on 5/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
